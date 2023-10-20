b0b978fd92546d0100d647b29de924aaaf022823
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2023-10-20 19:05:30 +0200

    Added Files

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-plateau-scurit-sret-1",
+      "name": "Plateau Sécurité Sûreté (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.2587027684739027,
+        "pitch": 0.20364448591407935,
+        "fov": 1.4337299731675692
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5559564746642067,
+          "pitch": 0.29081271833247513,
+          "rotation": 0,
+          "target": "1-plateau-scurit-sret-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-plateau-scurit-sret-2",
+      "name": "Plateau Sécurité Sûreté (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -3.1157976635328364,
+        "pitch": 0.29418448337441205,
+        "fov": 1.4337299731675692
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19261535867933688,
+          "pitch": 0.09775405823788219,
+          "rotation": 0,
+          "target": "0-plateau-scurit-sret-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
