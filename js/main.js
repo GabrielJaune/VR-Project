ac23d585b95598b96bdd4e21cc2d5c4b4ff0e34d
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2023-11-27 15:01:54 +0100

    VR-Test

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ const Infos = {
 }
 
 var Buttons = {}
+var ClickCooldown = 0
 
 AFRAME.registerComponent("name", {
     init: function() {
@@ -31,9 +32,11 @@ AFRAME.registerComponent("info-panel", {
     init: function() {
         this.el.sceneEl.renderer.sortObjects = true;
         this.el.object3D.renderOrder = 100;
-        console.log("OK BUDDY")
+
         this.onInsideClick  = this.onInsideClick.bind(this);
         this.onOutsideClick = this.onOutsideClick.bind(this);
+        this.OnBody = this.OnBody.bind(this);
+
         // NOTE: Not Outside Since Loading Stuff
         Buttons= document.querySelectorAll(".menu-button")
 
@@ -48,9 +51,12 @@ AFRAME.registerComponent("info-panel", {
             console.log(Buttons[i])
             Buttons[i].addEventListener("click", this.onInsideClick)
         }
+
+        document.body.addEventListener("click", this.OnBody)
     },
 
     onInsideClick: function (evt) {
+        ClickCooldown = Date.now() + 500
         var CurrentInfo = Infos[evt.currentTarget.id]
         this.el.object3D.visible = true
         var obj = [this.OldSize["x"], this.OldSize["y"], this.OldSize["z"]]
@@ -62,10 +68,53 @@ AFRAME.registerComponent("info-panel", {
     },
 
     onOutsideClick: function (evt) {
-        console.log("Outside Click")
+        if(ClickCooldown > Date.now()) { console.log("Cooldown"); return}
         this.el.object3D.visible = false
         this.el.object3D.scale.set(0, 0, 0)
+    },
+
+    OnBody: function(evt) {
+        console.log(evt.target)
+        if (evt.target.id == this.el.id) { return }
+        this.onOutsideClick()
     }
 })
+
+AFRAME.registerComponent('highlight', {
+    init: function () {
+      var buttonEls = this.buttonEls = this.el.querySelectorAll('.menu-button');
+      this.onMouseEnter = this.onMouseEnter.bind(this);
+      this.onMouseLeave = this.onMouseLeave.bind(this);
+      this.reset = this.reset.bind(this);
+      for (var i = 0; i < buttonEls.length; ++i) {
+        buttonEls[i].addEventListener('mouseenter', this.onMouseEnter);
+        buttonEls[i].addEventListener('mouseleave', this.onMouseLeave);
+      }
+    },
+  
+    onMouseEnter: function (evt) {
+      var buttonEls = this.buttonEls;
+      evt.target.setAttribute('material', 'color', '#046de7');
+      for (var i = 0; i < buttonEls.length; ++i) {
+        if (evt.target === buttonEls[i]) { continue; }
+        buttonEls[i].setAttribute('material', 'color', 'white');
+      }
+    },
+  
+    onMouseLeave: function (evt) {
+      if (this.el.is('clicked')) { return; }
+      evt.target.setAttribute('material', 'color', 'white');
+    },
+  
+    reset: function () {
+      var buttonEls = this.buttonEls;
+      for (var i = 0; i < buttonEls.length; ++i) {
+        this.el.removeState('clicked');
+        buttonEls[i].play();
+        buttonEls[i].emit('mouseleave');
+      }
+    }
+  });
+  
 
 console.log("Loaded Elemnt yay")
