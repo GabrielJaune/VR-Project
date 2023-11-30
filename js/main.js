4421e763b99f482faa22d8051029b58de77d2b44
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2023-11-30 10:33:30 +0100

    SN Update

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const Infos = {
     2: {
         title: "Bac Pro SN (Systeme Numerique)",
         info: "Le baccalaureat professionnel SN a pour ambition de former les futurs professionnels de l’informatique.",
-        redirect: "index.html"
+        redirect: "sn.html"
     },
     3: {
         title: "BTS Electrotechnique",
@@ -27,10 +27,10 @@ let PathName = location.pathname.split("/")
 PathName = PathName[PathName.length - 1].split(".")[0]
 
 function SwitchArea(Name) {
-  console.log($("#transition")[0].radius)
-  $("#transition").animate({
-    radius: "5"
-  }, 1000)
+  // console.log($("#transition")[0].radius)
+  // $("#transition").animate({
+  //   radius: "5"
+  // }, 1000)
   console.log("Changing To Area => " + Name)
   $("#MainScene")[0].attributes.template.nodeValue = "src: " + "./resources/pages/" + PathName + "/" + Name + ".html"
 
