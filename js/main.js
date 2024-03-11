0e12dabc895a5c469dc65feef4b64e03bce9d3a9
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2024-03-11 12:49:33 +0100

    Test

--- js/main.js
+++ js/main.js
@@ -67,6 +67,21 @@ const Infos = {
   }
 }
 
+AFRAME.registerComponent("character-controller", {
+  events: {
+    "navigation-start": function () {
+      if (this.el.hasAttribute("simple-navmesh-constraint")) {
+        this.el.setAttribute("simple-navmesh-constraint", "enabled", false);
+      }
+    },
+    "navigation-end": function () {
+      if (this.el.hasAttribute("simple-navmesh-constraint")) {
+        this.el.setAttribute("simple-navmesh-constraint", "enabled", true);
+      }
+    },
+  },
+});
+
 AFRAME.registerComponent("controller", {
   init: function() {
     this.onDown = this.onDown.bind(this)
